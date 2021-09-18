adae4121fd13033a823bea577096004cc460755a
Eric Miller <emiller@lirio.co>
2021-09-18 16:41:08 -0400

    Chapter 1 and 2 passed

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,9 @@
 use proc_macro::TokenStream;
+use syn::{parse_macro_input, DeriveInput};
+use quote::quote;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
-
-    unimplemented!()
+    let _ast = parse_macro_input!(input as DeriveInput);
+    quote!().into()
 }
